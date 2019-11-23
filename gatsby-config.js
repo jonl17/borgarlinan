25ac94436000cd76ff38137b015f82f3ccd4ebc7
Nonni Linux <jglorange@gmail>
2019-11-23 13:00:08 +0000

    bleo

--- gatsby-config.js
+++ gatsby-config.js
@@ -86,7 +86,6 @@ module.exports = {
         includedRoutes: [
           "**/media",
           "**/posts",
-          "**/qanda",
           "**/news",
           "**/starfsfolk",
           "**/pages",
