8e317d8b6beb27fbb755509318e60a506b1c03e9
Nonni Linux <jglorange@gmail>
2019-11-24 10:38:12 +0000

    cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -86,7 +86,6 @@ module.exports = {
         includedRoutes: [
           "**/media",
           "**/posts",
-          "**/news",
           "**/starfsfolk",
           "**/pages",
           "**/hopar",
