3966ab4c8cbead4dcce1a188c32756f11ba61903
Nonni Linux <jglorange@gmail>
2020-01-21 13:35:10 +0000

    progress

--- src/components/skyrslur/components/grid/components/CategoryFilter/Styled/index.js
+++ src/components/skyrslur/components/grid/components/CategoryFilter/Styled/index.js
@@ -3,6 +3,11 @@ import { styles } from "../../../../../../../constants"
 
 export const CategoryBox = styled.div`
   width: 350px;
+  height: ${props => props.height};
   background: ${styles.Blue};
 `
-export const Item = styled.p``
+export const Item = styled.p`
+  height: ${props => props.itemheight};
+  margin: 0;
+  color: white;
+`
--- src/components/skyrslur/components/grid/components/CategoryFilter/index.js
+++ src/components/skyrslur/components/grid/components/CategoryFilter/index.js
@@ -1,10 +1,36 @@
 import React from "react"
+import { graphql, StaticQuery } from "gatsby"
 
 /** components */
 import { CategoryBox, Item } from "./Styled"
 
-const CategoryFilter = () => {
-  return <CategoryBox></CategoryBox>
+const CategoryFilter = ({
+  data: {
+    site: {
+      siteMetadata: { utgefid_efni_filter_items },
+    },
+  },
+}) => {
+  return (
+    <CategoryBox height={utgefid_efni_filter_items.length * 50 + "px"}>
+      {utgefid_efni_filter_items.map(item => (
+        <Item itemheight={50 + "px"}>{item}</Item>
+      ))}
+    </CategoryBox>
+  )
 }
 
-export default CategoryFilter
+export default props => (
+  <StaticQuery
+    query={graphql`
+      {
+        site {
+          siteMetadata {
+            utgefid_efni_filter_items
+          }
+        }
+      }
+    `}
+    render={data => <CategoryFilter data={data} {...props}></CategoryFilter>}
+  ></StaticQuery>
+)
